08adbb5f7e372e3d577a7969af538a6b99021d25
MaNemoj01 <44163914+MaNemoj01@users.noreply.github.com>
2018-12-09 05:43:17 +0100

    Grad

---
+++ src/etf/unsa/ba/Grad.java
@@ -0,0 +1,72 @@
+package etf.unsa.ba;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.function.DoubleFunction;
+import java.util.stream.Collectors;
+
+public class Grad implements Serializable {
+    private String naziv;
+    private int brojStanovnika;
+    private double[] temperature = new double[1000];
+    private int brojMjerenja=0;
+
+    public Grad() {
+    }
+
+    public Grad(String naziv,  double[] temperature,int brojMjerenja) {
+        this.naziv = naziv;
+        this.brojStanovnika = 0;
+        this.brojMjerenja=brojMjerenja;
+        this.setTemperature(temperature,brojMjerenja);
+    }
+
+    public Grad(String naziv, double[] temperature, int brojMjerenja, int brojStanovnika){
+        this.naziv=naziv;
+        this.brojMjerenja=brojMjerenja;
+        setTemperature(temperature,brojMjerenja);
+        this.brojStanovnika=brojStanovnika;
+    }
+    public int getBrojMjerenja() {
+        return brojMjerenja;
+    }
+
+    public void setBrojMjerenja(int brojMjerenja) {
+        this.brojMjerenja = brojMjerenja;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature.clone();
+    }
+
+    public void setTemperature(double[] temperature,int brojMjerenja) {
+        if(temperature!=null)
+            System.arraycopy(temperature, 0, this.temperature, 0, brojMjerenja);
+    }
+
+    @Override
+    public String toString() {
+        String collect = Arrays.stream(temperature, 0, brojMjerenja).mapToObj(value -> Double.valueOf(value).toString()).collect(Collectors.joining(", "));
+        return
+                "Naziv = '" + naziv + '\'' +
+                        "\n  brojStanovnika = " + brojStanovnika +
+                        "\n  temperature = " + collect +
+                        "\n  brojMjerenja = " + brojMjerenja + "\n";
+    }
+}
