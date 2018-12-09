585a60aca760e3a011d79ff0dfd2da507fc1412a
MaNemoj01 <44163914+MaNemoj01@users.noreply.github.com>
2018-12-09 05:42:47 +0100

    Drzava

---
+++ src/etf/unsa/ba/Drzava.java
@@ -0,0 +1,73 @@
+package etf.unsa.ba;
+
+import java.io.Serializable;
+
+public class Drzava implements Serializable {
+    private String naziv;
+    private int brojStanovnika;
+    private double povrsina;
+    private String jedinicaPovrsine;
+    private Grad glavniGrad;
+
+    public Drzava() {
+    }
+
+    public Drzava(String naziv, int stanovnika, double povrsina, String jedinica, Grad glavniGrad) {
+        this.naziv=naziv;
+        this.brojStanovnika=stanovnika;
+        this.jedinicaPovrsine=jedinica;
+        this.glavniGrad= glavniGrad;
+        this.povrsina=povrsina;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public double getPovrsina() {
+        return povrsina;
+    }
+
+    public void setPovrsina(double povrsina) {
+        this.povrsina = povrsina;
+    }
+
+    public String getJedinicaPovrsine() {
+        return jedinicaPovrsine;
+    }
+
+    public void setJedinicaPovrsine(String jedinicaPovrsine) {
+        this.jedinicaPovrsine = jedinicaPovrsine;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
+
+    @Override
+    public String toString() {
+        return "Drzava{" +
+                "naziv='" + naziv + '\'' +
+                ", brojStanovnika=" + brojStanovnika +
+                ", povrsina=" + povrsina +
+                ", jedinicaPovrsine='" + jedinicaPovrsine + '\'' +
+                ", glavniGrad=" + glavniGrad +
+                '}';
+    }
+}
